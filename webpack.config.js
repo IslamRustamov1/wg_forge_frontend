e9616026cfa247f89ec93fe8cf7b05382e837865
islamrustamov1 <rustamov.exceedteam@gmail.com>
2020-04-22 13:44:24 +0300

    Configurate project

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const express = require("express");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fs = require('fs');
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: "./src/app.js", 
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
@@ -13,12 +14,38 @@ module.exports = {
       template: "./src/index.html"
     })
   ],
+  module:{
+    rules:[
+        {
+            test:/\.css$/,
+            use:['style-loader','css-loader']
+        }
+   ]
+},
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     open: true,
     port: 9000,
     before: function(app, server) {
       app.use("/api", express.static(path.join(__dirname, "data")));
+      app.get('/api/users', function (req, res) {
+        fs.readFile('./data/users.json', function (err, buffer) {
+          res.send(buffer);
+        })
+      });
+      app.get('/api/orders', function (req, res) {
+        fs.readFile('./data/orders.json', function (err, buffer) {
+          res.send(buffer);
+        })
+      });
+      app.get('/api/companies', function (req, res) {
+        fs.readFile('./data/companies.json', function (err, buffer) {
+          res.send(buffer);
+        })
+      });
     }
+  },
+  node: {
+    fs: 'empty'
   }
 };
